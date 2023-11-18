a627ae2e385b7d7efb704ed0ae6f87054a4c4f09
shamimfstack <shamimspy@gmail.com>
2023-11-18 22:18:00 +0600

    basic setup

---
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,12 @@
+
+
+const ErrorPage = () => {
+    return (
+        <div className="flex justify-center items-center min-h-screen">
+            <h2 className="text-4xl font-bold text-red-600">404</h2>
+            <h3 className="text-2xl font-bold text-red-500">Server Not Found</h3>
+        </div>
+    );
+};
+
+export default ErrorPage;
---
+++ src/Routes/Router.jsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter } from "react-router-dom";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
+import MainLayout from "../Layout/MainLayout";
+import Home from "../Pages/Home/Home/Home";
+import Register from "../Pages/Register/Register";
+
+
+const Router = createBrowserRouter([
+    {
+        path: '/',
+        errorElement: <ErrorPage></ErrorPage>,
+        element: <MainLayout></MainLayout>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: 'register',
+                element: <Register></Register>
+            }
+        ]
+    }
+])
+
+export default Router;
